2824598c3226cdfb82171c8dfe74ef50114edf7a
EzequielPires <ezequiel.pires082000@gmail.com>
2022-08-31 22:11:51 -0300

    Commit

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,5 +1,6 @@
 import { User } from "../../model/User";
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+import {v4 as uuidv4} from "uuid";
 
 class UsersRepository implements IUsersRepository {
   private users: User[];
@@ -19,23 +20,31 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+    user.name = name;
+    user.email = email;
+    user.created_at = new Date();
+    user.updated_at = new Date();
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find(user => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find(user => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    receivedUser.admin = true;
+    receivedUser.updated_at = new Date();
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,12 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userAlreadyExits = this.usersRepository.findByEmail(email);
+    if(userAlreadyExits) {
+      throw new Error("Mensagem do erro");
+    }
+    const user = this.usersRepository.create({email, name});
+    return user;
   }
 }
 
